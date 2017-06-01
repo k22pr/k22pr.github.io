ea47176a1d1170f0858cbce475f3fc5493c734e8
k22pr <adm@an1.kr>
2017-06-02 01:16:25 +0900

    vue load

--- js/myvue.js
+++ js/myvue.js
@@ -1,3 +1,17 @@
-var app = new Vue({
-  
-});
+new Vue({
+	el: '#root',
+	mounted() {
+		// make an ax request to our server
+
+		//axios
+		axios.get('./module').then(response => console.log(response));
+	}
+})
+
+langList = new Vue({
+	el: 'langList',
+	data: {
+		title: 'Language List',
+		content : '사용할 수 있거나 사용하고 있는 언어 목록입니다.'
+	}
+})
