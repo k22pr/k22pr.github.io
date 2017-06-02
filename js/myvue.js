d29a41e0ccc43b62f5fc7b7be00562ac7eb12ae3
k22pr <adm@an1.kr>
2017-06-02 08:31:11 +0000

    School Up

--- js/myvue.js
+++ js/myvue.js
@@ -1,17 +1,34 @@
+/*global Vue*/
 new Vue({
 	el: '#root',
 	mounted() {
 		// make an ax request to our server
 
 		//axios
-		axios.get('./module').then(response => console.log(response));
+		//axios.get('./content').then(response => console.log(response));
 	}
 })
 
-langList = new Vue({
-	el: 'langList',
+//langauge List Content
+new Vue({
+	el: '#langList',
 	data: {
-		title: 'Language List',
+		title: 'Web Language',
 		content : '사용할 수 있거나 사용하고 있는 언어 목록입니다.'
 	}
+})
+
+new Vue({
+		el : '#serverContent',
+		data : {
+			title : 'Server Side',
+			description : 'Hello!'
+		}
+})
+new Vue({
+		el : '#scriptContent',
+		data : {
+			title : 'Script',
+			description : 'Hello!'
+		}
 })
