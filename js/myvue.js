bf6abedd39570bb4d5f58a5ef637c94499232a67
k22pr <adm@an1.kr>
2017-06-05 10:08:28 +0000

    06-05 School UP

--- js/myvue.js
+++ js/myvue.js
@@ -1,4 +1,14 @@
+/*global langContent*/
+langContent = {
+	selectLang : 0,
+	lang : ['PHP7.x','JavaScript','CSS3','C lang','C#','Java','Rust'],
+	content : [
+		'PHP... ',
+		'js...'
+	]
+}
 /*global Vue*/
+/*
 new Vue({
 	el: '#root',
 	mounted() {
@@ -8,27 +18,34 @@ new Vue({
 		//axios.get('./content').then(response => console.log(response));
 	}
 })
+*/
 
 //langauge List Content
 new Vue({
 	el: '#langList',
 	data: {
-		title: 'Web Language',
-		content : '사용할 수 있거나 사용하고 있는 언어 목록입니다.'
+		title: 'Language List',
+		content : '사용할 수 있는 언어 목록 입니다.<br>PHP7.x, jQuery, Vue.js, Sass 등 다양한 웹언어를 다루며<br>서버부터 클라이언트에 걸쳐 다양한 언어를 다룰 수 있습니다.'
 	}
 })
 
-new Vue({
-		el : '#serverContent',
-		data : {
-			title : 'Server Side',
-			description : 'Hello!'
+var changeTabContent = new Vue({
+		el : '#tabContent',
+		computed : {
+			title : function(){
+				return langContent.lang[langContent.selectLang]
+			},
+			description : function(){
+				return langContent.content[langContent.selectLang]
+			}
 		}
 })
+
 new Vue({
-		el : '#scriptContent',
-		data : {
-			title : 'Script',
-			description : 'Hello!'
+	el : '#tabMenu',
+	data : {
+		changeTabMenu : function(no){
+			langContent.selectLang = no;
 		}
+	}
 })
